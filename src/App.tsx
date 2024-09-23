ece259b9378cc0c64f1145db9bcba372fc6b77d3
ChristianDPeralta <christianperalta656@gmail.com>
2024-09-23 18:52:11 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div class="resume">
+      <header>
+        <h1>CHRISTIAN DEAN M. PERALTA</h1>
+        <h2>FUTURE FRONT-END DEVELOPER</h2>
+      </header>
+
+      <div class="content">
+        <section class="sidebar">
+          <div class="contact">
+            <h1>Contact</h1>
+            <p>
+              <strong>Email:</strong> cdmperalta.student@ua.edu.ph
+            </p>
+            <p>
+              <strong>Facebook:</strong> Christian Dean Peralta
+            </p>
+            <p>
+              <strong>Location:</strong> San Nicolas, San Fernando, Pampanga
+            </p>
+          </div>
+
+          <div class="education">
+            <h1>Education</h1>
+            <p>Bachelor of Science in Information Technology (Present)</p>
+          </div>
+
+          <div class="skills">
+            <h1>Skills</h1>
+            <ul>
+              <li>CSS</li>
+              <li>HTML</li>
+              <li>Python</li>
+              <li>MySQL</li>
+              <li>Bootstrap</li>
+              <li>JavaScript</li>
+            </ul>
+          </div>
+        </section>
+
+        <section class="main-content">
+          <div class="experience">
+            <h1>Experience Section</h1>
+            <h2>1st Year 2022-2023</h2>
+            <div class="yr">
+              <h4>Python and Figma</h4>
+              <h3>Projects</h3>
+              <ul>
+                <li>.</li>
+              </ul>
+            </div>
+
+            <div class="yr">
+              <h2>2nd Year 2023-2024</h2>
+              <h4>Javascript, MySQL, HTML and CSS</h4>
+              <h3>Projects</h3>
+              <ul>
+                <li>.</li>
+              </ul>
+            </div>
+
+            <div class="yr">
+              <h1>3rd Year 2024 (Present) </h1>
+              <h4>HTML, CSS, REACTJS, and Bootstrap</h4>
+              <p>Answer here</p>
+              <h3>Projects</h3>
+              <ul>
+                <li>.</li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
