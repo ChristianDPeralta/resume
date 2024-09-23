cec0ddedaf59c219d7e96e06a240aa3bdfe87858
ChristianDPeralta <christianperalta656@gmail.com>
2024-09-23 18:58:41 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div class="resume">
+    <div className="resume">
       <header>
         <h1>CHRISTIAN DEAN M. PERALTA</h1>
         <h2>FUTURE FRONT-END DEVELOPER</h2>
       </header>
 
-      <div class="content">
-        <section class="sidebar">
-          <div class="contact">
+      <div className="content">
+        <section className="sidebar">
+          <div className="contact">
             <h1>Contact</h1>
             <p>
               <strong>Email:</strong> cdmperalta.student@ua.edu.ph
@@ -23,12 +23,12 @@ export default function App() {
             </p>
           </div>
 
-          <div class="education">
+          <div className="education">
             <h1>Education</h1>
             <p>Bachelor of Science in Information Technology (Present)</p>
           </div>
 
-          <div class="skills">
+          <div className="skills">
             <h1>Skills</h1>
             <ul>
               <li>CSS</li>
@@ -41,11 +41,11 @@ export default function App() {
           </div>
         </section>
 
-        <section class="main-content">
-          <div class="experience">
+        <section className="main-content">
+          <div className="experience">
             <h1>Experience Section</h1>
             <h2>1st Year 2022-2023</h2>
-            <div class="yr">
+            <div className="yr">
               <h4>Python and Figma</h4>
               <h3>Projects</h3>
               <ul>
@@ -53,7 +53,7 @@ export default function App() {
               </ul>
             </div>
 
-            <div class="yr">
+            <div className="yr">
               <h2>2nd Year 2023-2024</h2>
               <h4>Javascript, MySQL, HTML and CSS</h4>
               <h3>Projects</h3>
@@ -62,7 +62,7 @@ export default function App() {
               </ul>
             </div>
 
-            <div class="yr">
+            <div className="yr">
               <h1>3rd Year 2024 (Present) </h1>
               <h4>HTML, CSS, REACTJS, and Bootstrap</h4>
               <p>Answer here</p>
